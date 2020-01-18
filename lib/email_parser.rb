1b514517397e2628a87759314db4f9e308d34ecd
Justin Branch <justinms@outlook.com>
2020-01-18 17:16:50 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,14 +4,16 @@
 # or whitespace (' ').
 
 class EmailAddressParser
+  # Creating an instance variable reader/writer
   attr_accessor :emails
   
+  # Upon initialization, store string in instance variable @emails
   def initialize(emails)
     self.emails = emails
   end 
   
+  # Parse these emails, returning a unique array
   def parse
     self.emails.split(/, | /).uniq
   end 
-    
 end 
